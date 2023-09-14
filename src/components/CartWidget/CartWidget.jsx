d6bd62ad71affe84222358f2a5e4e07a5a5fbc25
ivan navatta <ivannavatta@gmail.com>
2023-09-14 12:44:44 -0300

    first commit

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function CartWidget () {
+    return(
+        <img src="https://cdn-icons-png.flaticon.com/512/107/107831.png" alt="" />
+    )
+
+}
+
+export default CartWidget
