5c2ae36727d77d343fad2fc17d33688321ddbf22
ivan navatta <ivannavatta@gmail.com>
2023-11-01 10:32:40 -0300

    firebase

--- src/components/Filter/TaskList.jsx
+++ src/components/Filter/TaskList.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Producto from '../Producto/Producto'
 import Spinner from '../Spinner/Spinner'
 import '../steelheets/ItemList.css'
 import ProductsDetails from '../ProductsDetails/ProductsDetails'
+import { getFirestore, doc, getDoc, getDocs, collection } from 'firebase/firestore'
+const TaskList = () => {
+  const [data, setData] = useState([])
 
-const TaskList = ({ data }) => {
+  useEffect(() => {
+    const db = getFirestore()
+    const itemRef = collection(db, 'items')
+    getDocs(itemRef). 
+    then(res => setData(res.docs.map(doc => ({ id: doc.id, ...doc.data()})
+    ))
+    )
+  }, [])
   return (
     <div className={data.length > 0 ? 'list-group' : '' }>
   
